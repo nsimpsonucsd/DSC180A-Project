1435bf4474221defae66f69e7fae61553d5fec4c
nsimpsonucsd <nsimpson@ucsd.edu>
2022-11-23 15:50:16 -0800

    Edited run.py

--- run.py
+++ run.py
@@ -3,12 +3,12 @@ import numpy as np
 import sys
 
 def read_data(d):
-    if d == 'test'
-    try:
-        data=pd.read_csv('test/2_test.csv', index_col=0)
-        return data
-    except:
-        print('data read unsuccessful.')
+    if d == 'test':
+        try:
+            data=pd.read_csv('test/2_test.csv', index_col=0)
+            return data
+        except:
+            print('data read unsuccessful.')
 
 if __name__ == "__main__":
     print(read_data(sys.argv[1]))
